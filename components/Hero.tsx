6c1d601fca2924cd4588e78e7af80f377aa361d8
ulysse <ulysse.couchoud@epitech.eu>
2024-06-09 01:42:28 +0200

    [ADD] - basics

---
+++ components/Hero.tsx
@@ -0,0 +1,40 @@
+//rafce => créer un component fonctionnel
+
+import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/text-generate-effect'
+
+const Hero = () => {
+  return (
+    <div className='pb-20 pt-36'>
+        <div>
+        <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
+        <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple'/>
+        <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue'/>
+        </div>
+
+
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        </div>
+
+        <div className='flex justify-center relative my-20 z-10'>
+            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+                <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+                    This website was made with Next.js and TailwindCSS
+                </h2>
+                <TextGenerateEffect
+                    className='text-center text-[40px] md:text-5xl lg:text-6xl mt-4'
+                    words="Welcome to my page ! "
+                />
+                <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+                    My name is Ulysse Couchoud and I'm a web developer who loves to create beautiful and functional websites.
+                </p>
+            </div>
+        </div>
+    </div>
+    
+    
+  )
+}
+
+export default Hero
